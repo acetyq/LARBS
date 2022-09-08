7bf79b6b128ccc599f623b8d0606adb8f35e1cfb
acetyq <113084354+acetyq@users.noreply.github.com>
2022-09-08 15:07:37 +0200

    Revert "Update larbs.sh"
    This reverts commit 03f584f00ea1a1b131cc09f229081a3385709943.

--- larbs.sh
+++ larbs.sh
@@ -138,7 +138,7 @@ gitmakeinstall() {
 		--no-tags -q "$1" "$dir" ||
 		{
 			cd "$dir" || return 1
-			sudo -u "$name" git pull
+			sudo -u "$name" git pull --force origin master
 		}
 	cd "$dir" || exit 1
 	make >/dev/null 2>&1
